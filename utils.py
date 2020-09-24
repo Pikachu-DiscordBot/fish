d8bcfad4b08c30904f397d4d86fe75343c3537f3
Jamie <flare2399@gmail.com>
2020-09-24 15:44:15 +0100

    more normalization

--- utils.py
+++ utils.py
@@ -1,8 +1,7 @@
 import json
 
-
 with open(f"data.json", "r", encoding="utf-8") as f:
-        m = json.load(f)
+    m = json.load(f)
 
 data = {}
 type = ["key", "legendary", "epic", "rare", "uncommon", "common", "garbage"]
@@ -10,11 +9,11 @@ type = ["key", "legendary", "epic", "rare", "uncommon", "common", "garbage"]
 for rod in m:
     c_weights = [m[rod][weight] for weight in m[rod]]
     _sc = sum(weight for weight in c_weights)
-    n_weights = [c/_sc for c in c_weights]
+    n_weights = [c / _sc for c in c_weights]
     for i, val in enumerate(n_weights):
         if rod not in data:
             data[rod] = {}
         data[rod][type[i]] = val
 
 with open(f"data.json", "w") as f:
-        f.write(json.dumps(data, indent=1))
+    f.write(json.dumps(data, indent=1))
